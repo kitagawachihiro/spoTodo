ed1ac02c1913e41490c8fd09cc94ef234ef42f9b
kitagawachihiro <ishikawachihiro0805@gmail.com>
2023-08-09 12:10:28 +0900

    userモデルにdistanceを追加、enumを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :todos, :dependent => :destroy
   has_many :spots, through: :todos
+
+  #0=1km, 1=3km, 2=5km, 3=10km
+  enum distance: { '0.621371': 0, '1.86411': 1, '3.10686': 2, '6.21371':3 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_09_030127) do
+ActiveRecord::Schema.define(version: 2023_08_09_030355) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2023_08_09_030127) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "distance", default: 0.0
   end
 
 end
