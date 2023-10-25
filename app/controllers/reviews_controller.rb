a4fdcc4feef9b6729f083649d715fe07669d3290
kitagawachihiro <ishikawachihiro0805@gmail.com>
2023-10-25 23:18:22 +0900

    flashメッセージのi18n対応

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
 
   def new
     if Review.exists?(todo_id: params[:todo_id])
-      redirect_back_or_to todos_path, danger: "既にレビューが作成されています。"
+      redirect_back_or_to todos_path, danger: t('notice.review.already')
     else
       @review = Review.new
     end
@@ -14,7 +14,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(rating: review_params[:rating], comment: review_params[:comment], todo_id: @todo.id)
     if @review.save
-      redirect_to todos_path, success: t('notice.review.create')
+      redirect_back_or_to todos_path, success: t('notice.review.create')
     else
       flash.now[:danger] = t('notice.review.not_create')
       render :new
@@ -25,18 +25,18 @@ class ReviewsController < ApplicationController
 
   def update
     if @review.update(rating: review_params[:rating], comment: review_params[:comment])
-      redirect_back_or_to achievedtodos_path, success: 'レビューを更新しました'
+      redirect_back_or_to achievedtodos_path, success: t('notice.review.update')
     else
-      flash.now[:danger] = '更新に失敗しました'
+      flash.now[:danger] = t('notice.review.not_update')
       render 'edit'
     end
   end
 
   def destroy
     if @review.destroy
-      redirect_back_or_to achievedtodos_path, success: 'レビューを削除しました'
+      redirect_back_or_to achievedtodos_path, success: t('notice.review.delete')
     else
-      flash.now[:danger] = '削除に失敗しました'
+      flash.now[:danger] = t('notice.review.not_delete')
       render 'edit'
     end
   end
