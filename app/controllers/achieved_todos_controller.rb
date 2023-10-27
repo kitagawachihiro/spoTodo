a1c189e46bc093b85a82e57b0ddb4e95dff6b923
kitagawachihiro <ishikawachihiro0805@gmail.com>
2023-10-28 00:05:24 +0900

    実行できたTodoが場所検索できるように設定

--- app/controllers/achieved_todos_controller.rb
+++ app/controllers/achieved_todos_controller.rb
@@ -1,5 +1,18 @@
 class AchievedTodosController < ApplicationController
   def index
-    @a_todos = current_user.todos.where(finished: TRUE).includes(:spot)
+   # @a_todos = current_user.todos.where(finished: TRUE).includes(:spot)
+
+
+    @q = Spot.ransack(params[:q])
+    @spots = @q.result(distinct: true)
+    @a_todos = []
+    
+    @spots.each do |spot|
+      spot.todos.each do |todo|
+        @a_todos << todo if current_user.todos.include?(todo) && todo.finished == TRUE
+      end
+    end
+
+    @a_todos = Kaminari.paginate_array(@a_todos).page(params[:page])
   end
 end
