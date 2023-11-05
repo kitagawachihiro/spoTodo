8c1b895b0223732fb8c8e719c73476d662683a41
kitagawachihiro <ishikawachihiro0805@gmail.com>
2023-11-05 15:56:05 +0900

    AdminLTE 導入、初期設定

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,6 @@
+//= require jquery3
+//= require jquery_ujs
+
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min       # 拡張子は省略可能
+//= require admin-lte/dist/js/adminlte.min
+//= require common.js
